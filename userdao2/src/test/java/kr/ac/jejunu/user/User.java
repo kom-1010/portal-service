4b5a08ee13fa5d5eb2c7e34a10061063d83fb7aa
kom-1010 <kom-1010@naver.com>
2020-04-10 19:37:07 +0900

    실습

---
+++ userdao2/src/test/java/kr/ac/jejunu/user/User.java
@@ -0,0 +1,31 @@
+package kr.ac.jejunu.user;
+
+public class User {
+    private Integer Id;
+    private String name;
+    private String password;
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
