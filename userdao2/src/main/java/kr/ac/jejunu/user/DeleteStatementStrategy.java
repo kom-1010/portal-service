f8c763df2c0f9ca77d75003f56a1c96792111c4d
kom-1010 <kom-1010@naver.com>
2020-05-01 16:24:54 +0900

    2단계 강의 실습

--- userdao2/src/main/java/kr/ac/jejunu/user/DeleteStatementStrategy.java
+++ userdao2/src/main/java/kr/ac/jejunu/user/DeleteStatementStrategy.java
@@ -6,9 +6,13 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DeleteStatementStrategy implements StatementStrategy {
+    private Integer id;
+    public DeleteStatementStrategy(Integer id){
+        this.id = id;
+    }
+
     @Override
-    public PreparedStatement makeStatement(Object object, Connection connection) throws SQLException {
-        Integer id = (Integer) object;
+    public PreparedStatement makeStatement(Connection connection) throws SQLException {
         PreparedStatement preparedStatement = connection.prepareStatement("delete from userinfo where id = ?", Statement.RETURN_GENERATED_KEYS);
         preparedStatement.setInt(1, id);
         return preparedStatement;
