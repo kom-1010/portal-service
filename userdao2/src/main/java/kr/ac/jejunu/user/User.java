92db01a7144df72c486067315f67a504a150f08e
kom-1010 <kom-1010@naver.com>
2020-05-22 13:04:11 +0900

    lombok 실습

--- userdao2/src/main/java/kr/ac/jejunu/user/User.java
+++ userdao2/src/main/java/kr/ac/jejunu/user/User.java
@@ -1,32 +1,13 @@
 package kr.ac.jejunu.user;
 
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
-    private Integer Id;
+    private Integer id;
     private String name;
     private String password;
-
-    public Integer getId() {
-        return Id;
-    }
-
-    public void setId(Integer id) {
-        Id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
 }
