e8977b0ecca6e8e42caa93efcf0bfbd638998a48
kom-1010 <kom-1010@naver.com>
2020-06-12 18:15:17 +0900

    두 번째 영상 실습

---
+++ userdao2/src/main/java/kr/ac/jejunu/user/RestController.java
@@ -0,0 +1,23 @@
+package kr.ac.jejunu.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/rest")
+@RequiredArgsConstructor
+public class RestController {
+    private final UserDao userDao;
+
+    @GetMapping("/{id}")
+    public @ResponseBody User get(@PathVariable("id") Integer id) {
+        return userDao.get(id);
+    }
+
+    @PostMapping
+    public @ResponseBody User create(@RequestBody User user) {
+        userDao.insert(user);
+        return user;
+    }
+}
